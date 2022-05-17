9305aa79802f1836a207264f25cd7df918313998
Rafael Goncalves <rsgoncalves@gmx.com>
2022-05-17 12:41:42 -0400

    Refactor to use parameterized SPARQL query files

--- hubmapy/hubmapy.py
+++ hubmapy/hubmapy.py
@@ -7,15 +7,9 @@ from py4j.java_gateway import JavaGateway, launch_gateway
 
 
 class HuBMAPy:
-    PREFIXES = "PREFIX ccf: <http://purl.org/ccf/>\n" + \
-               "PREFIX obo: <http://purl.obolibrary.org/obo/>\n" + \
-               "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" + \
-               "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" + \
-               "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + \
-               "PREFIX dc: <http://purl.org/dc/terms/>\n"
-    DEFAULT_ANATOMICAL_STRUCTURE = "obo:UBERON_0000006"
-    DEFAULT_CELL_TYPE = "obo:CL_0000171"
-    DEFAULT_TISSUE_BLOCK = "<http://dx.doi.org/10.1016/j.trsl.2017.07.006#TissueBlock>"
+    _DEFAULT_ANATOMICAL_STRUCTURE = "obo:UBERON_0000006"
+    _DEFAULT_CELL_TYPE = "obo:CL_0000171"
+    _DEFAULT_TISSUE_BLOCK = "<http://dx.doi.org/10.1016/j.trsl.2017.07.006#TissueBlock>"
 
     def __init__(self, output_folder=os.getcwd(), save_reasoned_ontology=False):
         launch_gateway(jarpath=os.path.dirname(os.path.abspath(__file__)) + '/resources/robot.jar',
@@ -31,22 +25,33 @@ class HuBMAPy:
             os.mkdir(output_folder)
 
     def _load_ontology(self, save_reasoned_ontology):
-        self._logger.info("Loading Ontology...")
+        self._logger.info("Loading ontology...")
         io_helper = self._gateway.jvm.org.obolibrary.robot.IOHelper()
         ontology = io_helper.loadOntology(os.path.dirname(os.path.abspath(__file__)) + '/resources/ccf.owl')
-
+        self._logger.info("...loaded HuBMAP ontology v" + self._get_ontology_version(ontology))
         self._logger.info("Reasoning over ontology...")
         reasoner_factory = self._gateway.jvm.org.semanticweb.elk.owlapi.ElkReasonerFactory()
         reason_operation = self._gateway.jvm.org.obolibrary.robot.ReasonOperation()
-        options_map = self._gateway.jvm.java.util.HashMap()
-        options_map.put("axiom-generators", "SubClass ClassAssertion PropertyAssertion")
-        reason_operation.reason(ontology, reasoner_factory, options_map)
-
+        reasoner_options = self._gateway.jvm.java.util.HashMap()
+        reasoner_options.put("axiom-generators", "SubClass ClassAssertion PropertyAssertion")
+        reason_operation.reason(ontology, reasoner_factory, reasoner_options)
+        self._logger.info("...done")
         if save_reasoned_ontology:
             iri = self._gateway.jvm.org.semanticweb.owlapi.model.IRI
             reasoned_ontology_iri = iri.create("file:" + self._output_folder + "/ccf_reasoned.owl")
             ontology.getOWLOntologyManager().saveOntology(ontology, reasoned_ontology_iri)
         return ontology
+
+    def _get_ontology_version(self, ontology):
+        ontology_version = ''
+        for annotation in ontology.getAnnotations():
+            if str(annotation.getProperty()) == "owl:versionInfo":
+                ontology_version = str(annotation.getValue().asLiteral().get().getLiteral())
+        return ontology_version
+
+    def load_query(self, query_file_name):
+        query_file = open(os.path.dirname(os.path.abspath(__file__)) + '/queries/' + query_file_name)
+        return query_file.read()
 
     def do_query(self, query, query_name='hubmap-query'):
         self._logger.debug("Executing query:\n" + query + "\n")
@@ -57,115 +62,55 @@ class HuBMAPy:
         self._query_operation.runQuery(self._dataset, query, query_results_file, output_format)
         return pd.read_csv(query_results_file_name)
 
-    # https://gist.github.com/rsgoncalves/c3dd13630807331bf571d4556fc4537f
     def biomarkers_for_all_cell_types(self):
-        query = self.PREFIXES + "SELECT ?ct ?ct_lbl ?marker ?marker_lbl WHERE {\n" + \
-                "?ct rdfs:subClassOf [ \n" + \
-                "  owl:onProperty obo:RO_0015004 ; \n" \
-                "  owl:someValuesFrom ?marker_set ] ;\n" + \
-                "  rdfs:label ?ct_lbl ;\n" + \
-                "  ccf:ccf_located_in ?as .\n" \
-                "  ?as rdfs:label ?as_lbl .\n" + \
-                "?marker_set owl:intersectionOf ?list .\n" + \
-                "?list rdf:rest*/rdf:first [\n" + \
-                "  owl:onProperty ccf:has_marker_component ; \n" + \
-                "  owl:someValuesFrom ?marker ] .\n" + \
-                "?marker rdfs:label ?marker_lbl }"
+        query = self.load_query('biomarkers_for_all_cell_types.rq')
         return self.do_query(query, query_name=self.biomarkers_for_all_cell_types.__name__)
 
-    # https://gist.github.com/rsgoncalves/b36dc685e4917593ac7c6a32cfd367d7
-    def biomarkers_for_all_cell_types_in_anatomical_structure(self, anatomical_structure=DEFAULT_ANATOMICAL_STRUCTURE):
-        query = self.PREFIXES + "SELECT (?ct as ?cell_type) (?ct_lbl as ?cell_type_name) ?marker (?marker_lbl as ?marker_name) WHERE {\n" + \
-                "?ct rdfs:subClassOf [\n" + \
-                "  owl:onProperty obo:RO_0015004 ;\n" + \
-                "  owl:someValuesFrom ?marker_set ] ;\n" + \
-                "  rdfs:label ?ct_lbl ;\n" + \
-                "  ccf:ccf_located_in " + anatomical_structure + " .\n" + \
-                "?marker_set owl:intersectionOf ?list .\n" + \
-                "?list rdf:rest*/rdf:first [\n" + \
-                "  owl:onProperty ccf:has_marker_component ; \n" + \
-                "  owl:someValuesFrom ?marker ] .\n" + \
-                "?marker rdfs:label ?marker_lbl }"
+    def biomarkers_for_all_cell_types_in_anatomical_structure(self, anatomical_structure=_DEFAULT_ANATOMICAL_STRUCTURE):
+        query = self.load_query('biomarkers_for_all_cell_types_in_anatomical_structure.rq')
+        query = query.replace("?anatomical_structure", anatomical_structure)
         return self.do_query(query, query_name=self.biomarkers_for_all_cell_types_in_anatomical_structure.__name__)
 
-    # https://gist.github.com/rsgoncalves/ffebc784cdfb306c729d221deb1c2074
-    def biomarkers_for_cell_type_in_anatomical_structure(self, cell_type=DEFAULT_CELL_TYPE,
-                                                         anatomical_structure=DEFAULT_ANATOMICAL_STRUCTURE):
-        query = self.PREFIXES + "SELECT ?ct_lbl ?as_lbl ?marker ?marker_lbl WHERE {\n" + \
-                cell_type + " rdfs:subClassOf [\n" + \
-                "  owl:onProperty obo:RO_0015004 ;\n" + \
-                "  owl:someValuesFrom ?marker_set ] ;\n" + \
-                "  rdfs:label ?ct_lbl ;\n" + \
-                "  ccf:ccf_located_in " + anatomical_structure + " .\n" + \
-                anatomical_structure + " rdfs:label ?as_lbl .\n" + \
-                "?marker_set owl:intersectionOf ?list .\n" + \
-                "?list rdf:rest*/rdf:first [\n" + \
-                "  owl:onProperty ccf:has_marker_component ; \n" + \
-                "  owl:someValuesFrom ?marker ] .\n" + \
-                "?marker rdfs:label ?marker_lbl }"
+    def biomarkers_for_cell_type_in_anatomical_structure(self, cell_type=_DEFAULT_CELL_TYPE,
+                                                         anatomical_structure=_DEFAULT_ANATOMICAL_STRUCTURE):
+        query = self.load_query('biomarkers_for_cell_type_in_anatomical_structure.rq')
+        query = query.replace("?cell_type", cell_type)
+        query = query.replace("?anatomical_structure", anatomical_structure)
         return self.do_query(query, query_name=self.biomarkers_for_cell_type_in_anatomical_structure.__name__)
 
-    # https://gist.github.com/rsgoncalves/2b450da4dd0cbb1dc13f8bf7929b6d2e
     # TODO refactor to obtain tissues that collide with parts of the anatomical structure
     def tissue_blocks_in_anatomical_structure(self, anatomical_structure="obo:UBERON_0000948"):
-        query = self.PREFIXES + "SELECT (?tb as ?tissue_block) ?donor ?donor_sex WHERE {\n" + \
-                "?tb rdf:type ccf:tissue_block ; \n" + \
-                "  ccf:has_registration_location [ ccf:collides_with " + anatomical_structure + " ] ; \n" + \
-                "  ccf:comes_from ?donor . \n" + \
-                "?donor ccf:has_biological_sex [ rdfs:label ?donor_sex ] }"
+        query = self.load_query('tissue_blocks_in_anatomical_structure.rq')
+        query = query.replace("?anatomical_structure", anatomical_structure)
         return self.do_query(query, query_name=self.tissue_blocks_in_anatomical_structure.__name__)
 
-    # https://gist.github.com/rsgoncalves/fe919eb2f33071df9da06d01469b5e41
     # TODO location of anatomical structure (filler of 'collides_with) is currently represented as a string literal
     def tissue_block_count_for_all_anatomical_structures(self):
-        query = self.PREFIXES + "SELECT (count(?tb) as ?tissue_block_count) ?as_lbl ?as WHERE { \n" + \
-                "?tb rdf:type ccf:tissue_block ; \n" + \
-                "  ccf:has_registration_location [ ccf:collides_with ?as ] . \n" + \
-                "BIND(IRI(?as) as ?as_iri) . \n" + \
-                "?as_iri rdfs:label ?as_lbl } \n" + \
-                "GROUP BY ?as ?as_lbl"
+        query = self.load_query('tissue_block_count_for_all_anatomical_structures.rq')
         return self.do_query(query, query_name=self.tissue_block_count_for_all_anatomical_structures.__name__)
 
-    def anatomical_structures_in_tissue_block(self, tissue_block=DEFAULT_TISSUE_BLOCK):
-        query = self.PREFIXES + "SELECT ?as ?as_lbl ?donor ?donor_sex WHERE { \n" + \
-                tissue_block + " rdf:type ccf:tissue_block ; \n" + \
-                "ccf:has_registration_location [ ccf:collides_with ?as ] ; \n" + \
-                "ccf:comes_from ?donor . \n" + \
-                "OPTIONAL { ?as rdfs:label ?as_lbl }\n" + \
-                "OPTIONAL { ?donor ccf:has_biological_sex [ rdfs:label ?donor_sex ] } }"
+    def anatomical_structures_in_tissue_block(self, tissue_block=_DEFAULT_TISSUE_BLOCK):
+        query = self.load_query('anatomical_structures_in_tissue_block.rq')
+        query = query.replace("?tissue_block", tissue_block)
         return self.do_query(query, query_name=self.anatomical_structures_in_tissue_block.__name__)
 
-    # https://gist.github.com/rsgoncalves/3d318acdba74e39ee8e92f0b367c1f5f
     def locations_of_all_cell_types(self):
-        query = self.PREFIXES + "SELECT ?ct ?ct_lbl ?as ?as_lbl WHERE { \n" + \
-                "?ct ccf:ccf_located_in ?as . \n" + \
-                "?ct rdfs:label ?ct_lbl . \n" + \
-                "?as rdfs:label ?as_lbl } "
+        query = self.load_query('locations_of_all_cell_types.rq')
         return self.do_query(query, query_name=self.locations_of_all_cell_types.__name__)
 
-    # https://gist.github.com/rsgoncalves/7181d9087a143085faa3f2b561d04051
-    def evidence_for_specific_cell_type(self, cell_type=DEFAULT_CELL_TYPE):
-        query = self.PREFIXES + "SELECT ?doi WHERE { \n" + \
-                "[ rdf:type owl:Axiom ; \n" + \
-                "  owl:annotatedSource " + cell_type + " ; \n" + \
-                "  owl:annotatedProperty rdfs:subClassOf ; \n" + \
-                "  dc:references ?doi ] }"
+    def evidence_for_specific_cell_type(self, cell_type=_DEFAULT_CELL_TYPE):
+        query = self.load_query('evidence_for_specific_cell_type.rq')
+        query = query.replace("?cell_type", cell_type)
         return self.do_query(query, query_name=self.evidence_for_specific_cell_type.__name__)
 
-    # https://gist.github.com/rsgoncalves/2268669a85e0a96d87e5a91a77be78f2
     def evidence_for_all_cell_types(self):
-        query = self.PREFIXES + "SELECT ?ct_lbl ?ct ?doi WHERE { \n" + \
-                "[ rdf:type owl:Axiom ; \n" + \
-                "  owl:annotatedSource ?ct ; \n" + \
-                "  owl:annotatedProperty rdfs:subClassOf ; \n" + \
-                "  dc:references ?doi ] . \n" + \
-                "?ct rdfs:label ?ct_lbl }"
+        query = self.load_query('evidence_for_all_cell_types.rq')
         return self.do_query(query, self.evidence_for_all_cell_types.__name__)
 
     @staticmethod
     def _logger():
         logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
         formatter = logging.Formatter("%(asctime)s %(levelname)s [%(name)s]: %(message)s", "%Y-%m-%d %H:%M:%S")
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(formatter)
--- test/test.py
+++ test/test.py
@@ -4,13 +4,11 @@ if __name__ == '__main__':
     hubmap = hubmapy.HuBMAPy()
 
     df_1 = hubmap.biomarkers_for_all_cell_types()
-    print(df_1.head())
-
-    # df_2 = query.biomarkers_for_all_cell_types_in_anatomical_structure(anatomical_structure='obo:UBERON_0002371')  # bone marrow
-    # df_3 = query.biomarkers_for_cell_type_in_anatomical_structure(cell_type='obo:CL_0000787', anatomical_structure='obo:UBERON_0002371')  # memory B cell, bone marrow
-    # df_4 = query.tissue_blocks_in_anatomical_structure(anatomical_structure='obo:UBERON_0002048')
-    # df_5 = query.tissue_block_count_for_all_anatomical_structures()
-    # df_6 = query.anatomical_structures_in_tissue_block(tissue_block='obo:UBERON_0002371')
-    # df_7 = query.locations_of_all_cell_types()
-    # df_8 = query.evidence_for_specific_cell_type(cell_type='obo:CL_0002394')  # i.e., publication DOIs
-    # df_9 = query.evidence_for_all_cell_types()
+    df_2 = hubmap.biomarkers_for_all_cell_types_in_anatomical_structure(anatomical_structure='obo:UBERON_0002371')  # bone marrow
+    df_3 = hubmap.biomarkers_for_cell_type_in_anatomical_structure(cell_type='obo:CL_0000787', anatomical_structure='obo:UBERON_0002371')  # memory B cell, bone marrow
+    df_4 = hubmap.tissue_blocks_in_anatomical_structure(anatomical_structure='obo:UBERON_0002048')
+    df_5 = hubmap.tissue_block_count_for_all_anatomical_structures()
+    df_6 = hubmap.anatomical_structures_in_tissue_block(tissue_block='<https://gtexportal.org/home/tissue/Lung#MTissueBlocks>')
+    df_7 = hubmap.locations_of_all_cell_types()
+    df_8 = hubmap.evidence_for_specific_cell_type(cell_type='obo:CL_0002394')  # i.e., publication DOIs
+    df_9 = hubmap.evidence_for_all_cell_types()
